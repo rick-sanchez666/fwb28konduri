95cd9e73edb993f3ba17c1e46ea931292476f696
Sai Deep Konduri <s544028@nwmissouri.edu>
2021-11-01 17:33:03 -0500

    mods endpoint added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const carRouter = require('./routes/car')
+const addModRouter = require('./routes/addmods')
 var app = express();
 
 // view engine setup
@@ -21,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/car', carRouter)
+app.use('/car', carRouter);
+app.use('/addmods', addModRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
